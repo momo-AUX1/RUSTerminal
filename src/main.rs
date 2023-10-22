61e0479892d20b26532f3a0843a9d919855a228a
momo-AUX1 <102610940+momo-AUX1@users.noreply.github.com>
2023-10-22 16:58:46 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use std::io::stdout;
 use std::process::exit;
 use std::env;
 use std::fs;
-//use tokio; last high level crate for a CURL alternative and async
-//use std::ptr; bare metal graphics version soon
+use rlua::Lua;
+use std::process;
 
-
-fn main() {
-    const VERSION: i8 = 2; 
+#[tokio::main]
+async fn main() {
+    const VERSION: i8 = 3; 
     loop {
         print!("# > ");
         stdout().flush().unwrap(); 
@@ -44,6 +44,34 @@ fn main() {
                 }
             },
             Some("ls") => ls(),
+            Some("exec") => {
+                if let Some(file_name) = args.first() {
+                    file_runner_handler(file_name);
+                } else {
+                    println!("run command needs a filename");
+                }
+            },
+            Some("run") => {
+                if let Some(file_name) = args.first() {
+                    file_runner_handler(file_name);
+                } else {
+                    println!("run command needs a filename");
+                }
+            },
+            Some("curl") => {
+                if let Some(url) = args.first() {
+                    request(url).await;
+                } else {
+                    println!("curl command needs a URL");
+                }
+            },
+            Some("req") => {
+                if let Some(url) = args.first() {
+                    request(url).await;
+                } else {
+                    println!("curl command needs a URL");
+                }
+            },
             Some("pwd") => {
                 let current_dir = pwd(); 
                 println!("{}", current_dir); 
@@ -64,6 +92,10 @@ fn help(){
     println!("ls - Lists the files in the current directory");
     println!("pwd - Shows the current directory");
     println!("cat - Reads a file");
+    println!("exec - Runs a file");
+    println!("run - Runs a file");  
+    println!("curl - makes a GET request to a URL");
+    println!("req - makes a GET request to a URL");
     println!("write - Writes to a file");
     println!("version or -v - Shows the version");
     println!("clear - Clears the screen");
@@ -111,5 +143,41 @@ fn write_file(file_name: &str) {
     match fs::write(full_path, contents) {
         Ok(_) => println!("File written successfully"),
         Err(err) => println!("Error writing file: {}", err),
+    }
+}
+
+fn file_runner_handler(handler: &str) {
+    let ext : Vec<&str> = handler.split(".").collect();
+    if ext[1] == "sh" || ext[1] == "bash" {
+        println!("Running Bash file... {}", handler);
+        let out = process::Command::new("bash").arg(handler).output().expect("Running .sh/.bash file");
+        print!("{}", String::from_utf8_lossy(&out.stdout))
+    }
+    else if ext[1] == "lua" {
+        println!("Running Lua file... {}", handler);
+        lua_handler(handler);
+    }
+    else {
+        println!("Unknown file type. Supported files : .sh/.bash/.lua");
+    }
+}
+
+fn lua_handler(handler: &str) {
+    let file = fs::read(handler).unwrap();
+    let lua_command = String::from_utf8(file).unwrap();
+    let lua = Lua::new();
+    let _ = lua.context(
+        |lua_context|{
+            lua_context.load(&lua_command).exec()
+        }
+    );
+}
+
+async fn request(url: &str) {
+    if url.starts_with("http://") || url.starts_with("https://") {
+        let requests = reqwest::get(url).await.unwrap().text().await.unwrap();
+        println!("{:?}", requests)
+    } else {
+        println!("URL must start with http:// or https://");
     }
 }
